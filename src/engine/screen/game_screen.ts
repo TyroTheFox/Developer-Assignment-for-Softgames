782d723c8e34744c40320c8fc39dcc48050ca8e4
TyroTheFox <boyinthreeparts@gmail.com>
2026-03-17 16:54:12 +0000

    Commented the Game Screen Class

--- src/engine/screen/game_screen.ts
+++ src/engine/screen/game_screen.ts
@@ -17,8 +17,15 @@ export type GameScreenDimensions = {
     scaleAgainstValue: number
 }
 
+/** 
+ * @class
+ * 
+ * Game Screen
+ * Handles Scaling and the general On-Screen Elements, as well as initalises the App
+ */
 export class GameScreen {
     static #instance: GameScreen;
+
     public assetLoader: AssetLoader = AssetLoader.instance;
     public stageManager!: StageManager;
     public fpsCounter!: PIXI.Text;
@@ -27,9 +34,11 @@ export class GameScreen {
     public loadingScreen!: LoadingScreen;
     public staticOverlayContainer!: PIXI.Container;
 
+    // Minimum Expected Size of the In-Game Screen
     public minWidth = 1500;
     public minHeight = 800;
 
+    // Dimentions of the In-Game Screen
     public gameScreenDimensions: GameScreenDimensions = {
         width: window.innerWidth,
         height: window.innerHeight,
@@ -37,10 +46,19 @@ export class GameScreen {
         scaleAgainstValue: 1
     }
 
+    /**
+     * @private
+     * @constructor
+     */
     private constructor() {
         initDevtools({ app });
     }
 
+    /**
+     * @get
+     * @static
+     * @public
+     */
     public static get instance(): GameScreen {
         if (!GameScreen.#instance) {
             GameScreen.#instance = new GameScreen();
@@ -49,9 +67,16 @@ export class GameScreen {
         return GameScreen.#instance;
     }
 
+    /**
+     * Initialises the whole game
+     * 
+     * @async
+     */
     public async initGame() {
+        // Check if on mobile phone
         const isMobile = this.assetLoader.isMobile();
 
+        //Initialise app
         await app.init({ 
             background: "#000000", 
             resolution: window.devicePixelRatio * (isMobile ? 0.5 : 1),
@@ -73,6 +98,7 @@ export class GameScreen {
 
         app.stage.sortableChildren = true;
 
+        // FPS Counter
         this.fpsCounter = new PIXI.Text({
             x: this.gameScreenDimensions.width * 0.05,
             y: this.gameScreenDimensions.height * 0.05,
@@ -88,7 +114,7 @@ export class GameScreen {
         this.fpsCounter.zIndex = 1000;
         app.stage.addChild(this.fpsCounter);
 
-        // Static UI element Contaiiner
+        // Static UI Element Contaiiner
         this.staticOverlayContainer = new PIXI.Container({ label: "StaticOverlay", zIndex: 999 });
         app.stage.addChild(this.staticOverlayContainer);
 
@@ -104,6 +130,7 @@ export class GameScreen {
             }
         });
 
+        // Full Screen Click Notice elements
         this.clickScreenNotice.pivot.x = this.clickScreenNotice.width * 0.5;
 
         this.clickScreenNotice.zIndex = 1000;
@@ -121,6 +148,12 @@ export class GameScreen {
         this.clickScreenOverlayGraphic.zIndex = 999;
     }
 
+    /**
+     * Requests the Game to go to Full Screen
+     * 
+     * @returns {void}
+     * @public
+     */
     public setFullScreen() {        
         if(this.isFullScreen()) {
             return;
@@ -141,6 +174,12 @@ export class GameScreen {
         this.resize();
     }
 
+    /**
+     * Checks if currently in Full Screen mode
+     * 
+     * @returns {boolean}
+     * @public 
+     */
     public isFullScreen(): boolean {
         // Check for programmatic full screen (Fullscreen API)
         const programmaticFullScreen = !!document.fullscreenElement;
@@ -155,6 +194,14 @@ export class GameScreen {
         return programmaticFullScreen || userTriggeredFullScreen;
     }
 
+    /**
+     * Loads all the Game Scenes and starts the game proper
+     * 
+     * @public
+     * @async
+     * @param {sceneListPair[]} sceneList 
+     * @param {stageListPair[]} stageList 
+     */
     public async loadGame(sceneList: sceneListPair[], stageList: stageListPair[]) {
         this.stageManager = StageManager.instance();
         await this.stageManager.init(sceneList, stageList, app.stage);
@@ -162,6 +209,11 @@ export class GameScreen {
         this.loadingScreen.hideLoadingScreen();
     }
 
+    /**
+     * Begins updating Scenes
+     * 
+     * @public
+     */
     public beginUpdateLoop() {
         // Listen for animate update
         app.ticker.add((time) => {
@@ -170,11 +222,23 @@ export class GameScreen {
         });
     }
 
+    /**
+     * Changes a Staged Scene for a new Registered One
+     * 
+     * @param {string} stageName 
+     * @param {string} sceneName 
+     */
     public async changeScene(stageName: string, sceneName: string) {
         await this.stageManager.changeScene(stageName, sceneName);
     }
 
+    /**
+     * Resizes the In-Game Screen and rearranges the assets for everything else
+     * 
+     * @returns {{ width: number, height: number, scaleWithValue: number, scaleAgainstValue: number }}
+     */
     public resize(): { width: number, height: number, scaleWithValue: number, scaleAgainstValue: number } {
+        // Check for Full Screen to see if the Notice can come down
         if (this.clickScreenNotice) {
             this.clickScreenNotice.visible = !this.isFullScreen();
             this.clickScreenOverlayGraphic.visible = !this.isFullScreen();
